f0b45a6751ddbe3549e845d776c77dd23c746f18
dukim <kimdw9983@gmail.com>
2022-07-26 13:40:52 +0900

    0726

--- src/main/java/com/oembed/main/OEmbedController.java
+++ src/main/java/com/oembed/main/OEmbedController.java
@@ -2,28 +2,19 @@ package com.oembed.main;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONObject;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class OEmbedController {
@@ -58,83 +49,62 @@ public class OEmbedController {
     public ResponseEntity<OEmbedMessage> oembed(HttpServletRequest request) {
 		String requestURL = request.getRequestURL().toString();
 		String url = requestURL.split("/oembed/")[1];
-		String queryString = request.getQueryString();
+		String query = request.getQueryString();
+		if (query != null) url = url + "?" + query;
 		
 		OEmbedMessage message = new OEmbedMessage();
         HttpHeaders headers= new HttpHeaders();
         headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
         
-        if(!OEmbedService.validateURL(url)) {
-            message.setStatus(HttpStatus.FORBIDDEN);
+        if(!OEmbedService.validateURL(url)) { //url 유효성 체크
+            message.setStatus(HttpStatus.BAD_REQUEST);
             message.setData("유효하지 않은 url입니다.");
             
-            return new ResponseEntity<>(message, headers, HttpStatus.FORBIDDEN);
+            return new ResponseEntity<>(message, headers, HttpStatus.BAD_REQUEST);
         };
         
         String provider;
-        try {
+        try { //url에서 oembed요청을 보낼 provider 탐색
         	provider = OEmbedService.getProvider(url);
         } catch (RuntimeException e) {
-        	message.setStatus(HttpStatus.FORBIDDEN);
+        	message.setStatus(HttpStatus.BAD_REQUEST);
             message.setData(e.getMessage());
             
-            return new ResponseEntity<>(message, headers, HttpStatus.FORBIDDEN);
+            return new ResponseEntity<>(message, headers, HttpStatus.BAD_REQUEST);
         }
         
-        try {
-        	provider = "https://www.youtube.com/oembed?url=";
-        	System.out.println(url);
-        	System.out.println(queryString);
-        	System.out.println(provider + url + "?" +  queryString);        	
-			HttpResponse response = HttpClientBuilder.create().build().execute(new HttpGet("https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v=dBD54EZIrZo"));
-			 
-			String json_string = EntityUtils.toString(response.getEntity(), "UTF-8");
-			JSONObject temp1 = new JSONObject(json_string);
-			message.setData(json_string);
-			//message.setStatus(HttpStatus.OK);
-            //message.setData(oembedInfo);
-            
+    	Map<String, String> data = null;
+		try {
+			data = OEmbedService.getOembedData(provider, url);
+		} catch (ParseException e) {
+			message.setStatus(HttpStatus.NO_CONTENT);
+	        message.setData(e.getMessage());
+	        
+	        return new ResponseEntity<>(message, headers, HttpStatus.INTERNAL_SERVER_ERROR);
 		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
+			message.setStatus(HttpStatus.NOT_IMPLEMENTED);
+            message.setData(e.getMessage());
+            
+            return new ResponseEntity<>(message, headers, HttpStatus.NOT_IMPLEMENTED);
+		}  catch (IOException e) {
+			message.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+            message.setData(e.getMessage());
+            
+            return new ResponseEntity<>(message, headers, HttpStatus.INTERNAL_SERVER_ERROR);
+		} 
+		
+        message.setStatus(HttpStatus.OK);
+		message.setData(data);
+		
         return new ResponseEntity<>(message, headers, HttpStatus.OK);
     }
 	
 	@GetMapping(value = "/test")
     public ResponseEntity<OEmbedMessage> test() {
-		HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-        factory.setConnectTimeout(5000); //타임아웃 설정 5초
-        factory.setReadTimeout(5000);//타임아웃 설정 5초
-        RestTemplate restTemplate = new RestTemplate(factory);
-		
 		OEmbedMessage message = new OEmbedMessage();
         HttpHeaders headers= new HttpHeaders();
         headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
-        HttpEntity<?> entity = new HttpEntity<>(headers);
-		try {
-			ResponseEntity<OEmbedMessage> result = restTemplate.exchange("http://127.0.0.1:8080/oembed/test", HttpMethod.GET, entity, OEmbedMessage.class);
-			message.setStatus(HttpStatus.FORBIDDEN);
-	        message.setData(result.getStatusCodeValue());
-	        
-		} catch (HttpClientErrorException e) {
-			message.setStatus(HttpStatus.FORBIDDEN);
-	        message.setData("잘못된 url");
-	        
-			return new ResponseEntity<>(message, headers, HttpStatus.FORBIDDEN);
-		}
-        
-        return new ResponseEntity<>(message, headers, HttpStatus.FORBIDDEN);
-    }
-	
-	@RequestMapping("/errorPage")
-    public ModelAndView errorPage(String reason) {
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("errorPage");
-        //mv.addObject("random_string", "asdjkfjasdkf");
 
-        return mv;
+        return new ResponseEntity<>(message, headers, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/oembed/main/OEmbedService.java
+++ src/main/java/com/oembed/main/OEmbedService.java
@@ -1,15 +1,40 @@
 package com.oembed.main;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Map;
 
 import org.apache.commons.validator.routines.UrlValidator;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class OEmbedService {
-	String[] schemes = {"http", "https"};
+	private final String[] schemes = {"http", "https"};
 	private final UrlValidator urlValidator = new UrlValidator(schemes);
+	private final ObjectMapper mapper = new ObjectMapper();
+	private JSONObject providers = null;
+	
+	public OEmbedService() {
+		providers = loadProviders("https://oembed.com/providers.json");
+	}
+	
+	public JSONObject loadProviders(String url) {
+		
+		return providers;
+	}
 	
 	public Boolean validateURL(String url) {
 		return urlValidator.isValid(url);
@@ -25,5 +50,43 @@ public class OEmbedService {
 		}
 		
 		return host;
+	}
+
+	@SuppressWarnings("unchecked")
+	public Map<String, String> getOembedData(String provider, String url) throws ClientProtocolException, IOException, ParseException {
+		provider = "https://www.youtube.com/oembed?url="; //TEST
+
+		HttpEntity entity = null;
+        try {
+        	HttpClient client = HttpClientBuilder.create().build();
+        	HttpGet httpget = new HttpGet(provider + url);
+			HttpResponse response = client.execute(httpget);
+			entity = response.getEntity();
+		} catch (IOException e) {
+			throw new ClientProtocolException("응답 데이터 수신중에 오류가 발생했습니다.");
+		}
+        if (entity == null) throw new ClientProtocolException("응답 데이터가 없습니다.");
+		
+        Map<String, String> data = null;
+    	try {
+			data = mapper.readValue(EntityUtils.toString(entity), Map.class);
+    	} catch (JsonParseException e){ //결과값이 Map이 아닐경우 (e.g, "Bad Request", "Not Found") readValue에 실패하면서 발생 
+    		throw new ParseException("해당 url에서 컨텐츠를 가져올 수 없습니다.");
+		} catch (IOException e) {
+			throw new IOException("응답 데이터를 처리하는중에 오류가 발생했습니다.");
+		}
+    	
+		/*
+		 * OEmbedData data = null; //POJO방식의 데이터로 바인딩 할 경우
+		 * try { 
+		 * data = new OEmbedData();
+		 * ObjectMapper mapper = new ObjectMapper(); 
+		 * data = mapper.readValue(EntityUtils.toString(entity), OEmbedData.class);
+		 * 
+		 * } catch (JSONException | ParseException | IOException e) { throw new
+		 * ParseException("응답 데이터를 처리하는중에 오류가 발생했습니다."); }
+		 */
+
+		return data;
 	}
 }
